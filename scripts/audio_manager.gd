9da69fc1d1cdfd2a39cfb9378d14b20007ce2bcb
milkyGUY19 <jaramach08@gmail.com>
2026-02-28 02:21:59 +0100

    Implement global SettingsManager for persistent options

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -6,10 +6,29 @@ var audio_bus_id
 
 func _ready() -> void:
 	audio_bus_id = AudioServer.get_bus_index(audio_bus_name)
-	var db = linear_to_db(value)
-	AudioServer.set_bus_volume_db(audio_bus_id, db)
+	
+	# Načíst uloženou hodnotu ze SettingsManageru při startu
+	if audio_bus_name == "Master":
+		value = SettingsManager.master_volume
+	elif audio_bus_name == "SFX":
+		value = SettingsManager.sfx_volume
+	elif audio_bus_name == "Music":
+		value = SettingsManager.music_volume
+		
+	# Připojit signál pro změny z UI
 	value_changed.connect(_on_value_changed)
 
 func _on_value_changed(val: float) -> void:
+	# Aplikovat hlasitost do AudioServeru
 	var db = linear_to_db(val)
 	AudioServer.set_bus_volume_db(audio_bus_id, db)
+	
+	# Uložit do SettingsManageru
+	if audio_bus_name == "Master":
+		SettingsManager.master_volume = val
+	elif audio_bus_name == "SFX":
+		SettingsManager.sfx_volume = val
+	elif audio_bus_name == "Music":
+		SettingsManager.music_volume = val
+		
+	SettingsManager.save_settings()
--- scripts/options_buttons.gd
+++ scripts/options_buttons.gd
@@ -1,6 +1,8 @@
 extends VBoxContainer
 
 @onready var resolution_option: OptionButton = $ResolutionOption
+@onready var fullscreen_checkbox: CheckBox = $Fullscreen
+@onready var vsync_checkbox: CheckBox = $VSync
 
 const RESOLUTIONS = [
 	Vector2i(2560, 1440),
@@ -14,33 +16,33 @@ func _ready() -> void:
 	for res in RESOLUTIONS:
 		resolution_option.add_item(str(res.x) + "x" + str(res.y))
 
-	# Try to find current resolution
-	var current_res = DisplayServer.window_get_size()
+	# Načíst stav UI ze SettingsManageru místo z výchozích hodnot scény
+	fullscreen_checkbox.button_pressed = SettingsManager.fullscreen
+	vsync_checkbox.button_pressed = SettingsManager.vsync
+	
+	# Vybrat správné rozlišení v dropdownu
+	var current_res = SettingsManager.resolution
 	for i in range(RESOLUTIONS.size()):
 		if RESOLUTIONS[i] == current_res:
 			resolution_option.select(i)
 			break
 
 func _on_fullscreen_toggled(toggled_on: bool) -> void:
-	if (toggled_on == true):
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	SettingsManager.fullscreen = toggled_on
+	SettingsManager.apply_settings()
+	SettingsManager.save_settings()
 
 func _on_v_sync_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
-	else:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	SettingsManager.vsync = toggled_on
+	SettingsManager.apply_settings()
+	SettingsManager.save_settings()
 
 func _on_resolution_option_item_selected(index: int) -> void:
 	var size = RESOLUTIONS[index]
-	DisplayServer.window_set_size(size)
+	SettingsManager.resolution = size
 	
-	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
-		var screen_id = DisplayServer.window_get_current_screen()
-		var screen_rect = DisplayServer.screen_get_usable_rect(screen_id)
-		var center_pos = screen_rect.position + (screen_rect.size / 2) - (size / 2)
-		DisplayServer.window_set_position(center_pos)
-
-
+	# Pokud jsme ve windowed módu, rovnou aplikujeme, jinak to bude vidět po vypnutí fullscreenu
+	if not SettingsManager.fullscreen:
+		SettingsManager.apply_settings()
+		
+	SettingsManager.save_settings()
